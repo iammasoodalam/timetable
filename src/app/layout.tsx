2d3e6ffb65912c4640990f724508a85aa3295b19
Mohammad Masood Alam <130masoodalam@gmail.com>
2023-12-15 16:08:12 +0530

    added title and favicon to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
 import '@/app/global.scss'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "Time Table for Aliah CSE 2nd year",
+  description: "This website will show the time table for Aliah University, CSE students. And if any lecture time is modified by the professor then you can modify it so."
+
+}
+
 export default function RootLayout({
   children,
 }: {
