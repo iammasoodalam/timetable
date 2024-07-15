31c177a2bdeac79c1186b7fe40db6c1becb58e0a
Mohammad Masood Alam <130masoodalam@gmail.com>
2024-07-16 00:55:53 +0530

    added types in the global scope

---
+++ src/types/global.d.ts
@@ -0,0 +1,21 @@
+type data = [
+  [
+    {
+      time_from: string;
+      time_to: string;
+      course_code: string;
+      course_name: string;
+      schedule_type: string;
+      professor: [string];
+    }
+  ]
+];
+
+type cardData = {
+  time_from: string;
+  time_to: string;
+  course_code: string;
+  course_name: string;
+  schedule_type: string;
+  professor: [string];
+};
