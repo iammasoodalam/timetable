414d036859627a632ec4efd0bcd9c5a33f11bbb3
Mohd Masood Alam <130masoodalam@gmail.com>
2024-07-18 16:52:25 +0530

    now app will run offline too

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+async function networkFirst(request) {
+  try {
+    const networkResponse = await fetch(request);
+    if (networkResponse.ok) {
+      const cache = await caches.open("MyCache_1");
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  } catch (error) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || Response.error();
+  }
+}
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (url.pathname.match("/")) {
+    event.respondWith(networkFirst(event.request));
+  }
+});
