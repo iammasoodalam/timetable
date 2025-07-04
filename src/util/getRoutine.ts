a93d1ef51e5e1472d9d5457b7774cf5c72a0830e
Mohd Masood Alam <130masoodalam@gmail.com>
2025-07-04 23:00:40 +0530

    semester 7 data is added

--- src/util/getRoutine.ts
+++ src/util/getRoutine.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 export default function getRoutine(): data {
   const routine = fs.readFileSync(
-    path.join(process.cwd(), "src/data/cse_sem_6.json"),
+    path.join(process.cwd(), "src/data/cse_sem_7.json"),
     "utf-8"
   );
   return JSON.parse(routine);
